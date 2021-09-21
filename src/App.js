91778c5452219c28028f70cb0e3d0a6eda093362
unknown <Piashdeb>
2021-09-22 01:51:21 +0600

    managed

--- src/App.js
+++ src/App.js
@@ -16,19 +16,7 @@ function App() {
     </div>
   );
 }
-/*
-
-      <Friend phone="01700000" address="noakhali"></Friend>
-      <Friend phone="01700001" address="rongpur"></Friend>
-      <Friend phone="01700002" address="dhaka"></Friend>
-      <Friend phone="01700003" address="Jessore"></Friend>
-<p id="title">Title</p>
-       <Person name = "Rubel" nayeka="Mousumi"></Person>
-      <Person name="BappaRaj" nayeka="Cheka"></Person>
-      <Person name="Kuber" nayeka="Kopila"></Person>
-      {/* <Person></Person>
-      <Person></Person>
-      <Person></Person>} */ 
+ 
  
 
 function Person(props) {                       // component
@@ -41,14 +29,7 @@ function Person(props) {                       // component
   }
   return (
 
-    // properties = props
-    // style={person}{
-    //   backgroundColor: 'skyblue',
-    //   border: '3px solid lightsalmon',         // style diye o use kora jay 
-    //   margin: '20px',                          // again class diye o use kora jay
-    //   borderRadius: '20px'
-
-    // }
+    
     <div style={person} >
       <h1>Name: {props.name}</h1>
       <h4>Hero Of: {props.nayeka}</h4>
@@ -66,3 +47,26 @@ function Friend(props){
 }
 
 export default App;
+
+
+/*
+
+      <Friend phone="01700000" address="noakhali"></Friend>
+      <Friend phone="01700001" address="rongpur"></Friend>
+      <Friend phone="01700002" address="dhaka"></Friend>
+      <Friend phone="01700003" address="Jessore"></Friend>
+<p id="title">Title</p>
+       <Person name = "Rubel" nayeka="Mousumi"></Person>
+      <Person name="BappaRaj" nayeka="Cheka"></Person>
+      <Person name="Kuber" nayeka="Kopila"></Person>
+      {/* <Person></Person>
+      <Person></Person>
+      <Person></Person>} */
+      // properties = props
+    // style={person}{
+    //   backgroundColor: 'skyblue',
+    //   border: '3px solid lightsalmon',         // style diye o use kora jay 
+    //   margin: '20px',                          // again class diye o use kora jay
+    //   borderRadius: '20px'
+
+    // }
